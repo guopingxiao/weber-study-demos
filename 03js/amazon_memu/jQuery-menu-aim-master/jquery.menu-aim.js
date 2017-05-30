cfe2404524071eb54cd78963826a982ba6865311
xiaoguoping <xiaoguoping@meituan.com>
2017-05-30 23:51:25 +0800

    add css制作旋转圈

--- 03js/amazon_memu/jQuery-menu-aim-master/jquery.menu-aim.js
+++ 03js/amazon_memu/jQuery-menu-aim-master/jquery.menu-aim.js
@@ -1,30 +1,4 @@
 /**
- * menu-aim is a jQuery plugin for dropdown menus that can differentiate
- * between a user trying hover over a dropdown item vs trying to navigate into
- * a submenu's contents.
- *
- * menu-aim assumes that you have are using a menu with submenus that expand
- * to the menu's right. It will fire events when the user's mouse enters a new
- * dropdown item *and* when that item is being intentionally hovered over.
- *
- * __________________________
- * | Monkeys  >|   Gorilla  |
- * | Gorillas >|   Content  |
- * | Chimps   >|   Here     |
- * |___________|____________|
- *
- * In the above example, "Gorillas" is selected and its submenu content is
- * being shown on the right. Imagine that the user's cursor is hovering over
- * "Gorillas." When they move their mouse into the "Gorilla Content" area, they
- * may briefly hover over "Chimps." This shouldn't close the "Gorilla Content"
- * area.
- *
- * This problem is normally solved using timeouts and delays. menu-aim tries to
- * solve this by detecting the direction of the user's mouse movement. This can
- * make for quicker transitions when navigating up and down the menu. The
- * experience is hopefully similar to amazon.com/'s "Shop by Department"
- * dropdown.
- *
  * Use like so:
  *
  *      $("#menu").menuAim({
@@ -38,11 +12,9 @@
  * the relevant row's HTML element as the execution context ('this'):
  *
  *      .menuAim({
- *          // Function to call when a row is purposefully activated. Use this
- *          // to show a submenu's content for the activated row.
+ *          
  *          activate: function() {},
  *
- *          // Function to call when a row is deactivated.
  *          deactivate: function() {},
  *
  *          // Function to call when mouse enters a menu row. Entering a row
@@ -99,7 +71,7 @@
             }, opts);
 
         var MOUSE_LOCS_TRACKED = 3,  // number of past mouse locations to track
-            DELAY = 300;  // ms delay when user appears to be entering submenu
+            DELAY = 300;  // ms 当用户有趋势去二级导航就延时300
 
         /**
          * Keep track of the last few locations of the mouse.
@@ -263,7 +235,7 @@
                 // down the screen, we are looking for the slope between the
                 // cursor and the upper right corner to decrease over time, not
                 // increase (somewhat counterintuitively).
-                function slope(a, b) {
+                function slope(a, b) { //倾斜角
                     return (b.y - a.y) / (b.x - a.x);
                 };
 
