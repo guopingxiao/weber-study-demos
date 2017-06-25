394913e50576dad3bf7caab241300a9c2549c2fb
apple <i10630226@163.com>
2017-06-26 01:55:18 +0800

    add chengmo 深入浅出redux

---
+++ 06react/chengmo_react_redux/react-and-redux-master/chapter-03/react-redux/src/views/Counter.js
@@ -0,0 +1,44 @@
+import React, { PropTypes } from 'react';
+import * as Actions from '../Actions.js';
+import {connect} from 'react-redux';
+
+const buttonStyle = {
+  margin: '10px'
+};
+
+function Counter({caption, onIncrement, onDecrement, value}) {
+  return (
+    <div>
+      <button style={buttonStyle} onClick={onIncrement}>+</button>
+      <button style={buttonStyle} onClick={onDecrement}>-</button>
+      <span>{caption} count: {value}</span>
+    </div>
+  );
+}
+
+Counter.propTypes = {
+  caption: PropTypes.string.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired,
+  value: PropTypes.number.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    value: state[ownProps.caption]
+  }
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    onIncrement: () => {
+      dispatch(Actions.increment(ownProps.caption));
+    },
+    onDecrement: () => {
+      dispatch(Actions.decrement(ownProps.caption));
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+
